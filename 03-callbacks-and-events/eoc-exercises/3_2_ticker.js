b860f9876ce1f16982a960262523bab5b0f46c55
Jack Barry <jackaldenbarry@gmail.com>
2020-10-29 11:29:35 -0500

    Answer Exercise 3.2

---
+++ 03-callbacks-and-events/eoc-exercises/3_2_ticker.js
@@ -0,0 +1,31 @@
+import { EventEmitter } from 'events'
+
+function tick (emitter, number, tickCount, finished) {
+  setTimeout(() => {
+    if (number <= 0) {
+      return finished(tickCount)
+    }
+
+    tickCount++
+    emitter.emit('tick')
+    tick(emitter, number - 50, tickCount, finished)
+  }, 50)
+}
+
+function ticker (number, callback) {
+  const emitter = new EventEmitter()
+
+  tick(emitter, number, 0, callback)
+
+  return emitter
+}
+
+const cliTickCount = process.argv[2]
+
+if (cliTickCount > 9999) {
+  console.error("Please keep your ms under 10000 otherwise you'll be sitting here a while")
+  process.exit(1)
+}
+
+ticker(cliTickCount, (tickCount) => console.log(`Finished with ${tickCount} ticks`))
+  .on('tick', () => console.log('Tick'))
